b070900af0edc1e01b60a4a72b0ccd6514083173
FocJoe <FocilvJOey@foxmail.com>
2018-12-04 21:12:19 +0800

    Test Commit

--- app/src/main/java/com/focjoe/roucator/MainActivity.java
+++ app/src/main/java/com/focjoe/roucator/MainActivity.java
@@ -1,5 +1,6 @@
 package com.focjoe.roucator;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent();
     }
 }
