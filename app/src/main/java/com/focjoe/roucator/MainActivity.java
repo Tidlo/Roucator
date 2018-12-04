7eb1e9b888f1d8afd2f39ca3428e82756f08d975
FocJoe <FocilvJOey@foxmail.com>
2018-12-04 21:36:30 +0800

    Commit to develop branch

--- app/src/main/java/com/focjoe/roucator/MainActivity.java
+++ app/src/main/java/com/focjoe/roucator/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Intent intent = new Intent();
         intent.putExtra("test", "ssdfsdf");
+        intent.putExtra("branch", "branch");
     }
 }
