689e3ded2213a40e6d61f1aa4bafa5c9b25c2e81
FocJoe <FocilvJOey@foxmail.com>
2018-12-05 16:54:41 +0800

    Added Drawer

--- app/src/main/java/com/focjoe/roucator/MainActivity.java
+++ app/src/main/java/com/focjoe/roucator/MainActivity.java
@@ -1,17 +1,42 @@
 package com.focjoe.roucator;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent();
-        intent.putExtra("test", "ssdfsdf");
-        intent.putExtra("branch", "branch");
+        toolbar = findViewById(R.id.main_tool_bar);
+//        使用 toolbar 取代 actionbar
+        setSupportActionBar(toolbar);
+
+
+    }
+
+
+    //重写下面的两个方法来使用自定义 toolbar 上的菜单，定义菜单的的点击事件
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.toolbar_menu_refresh:
+                Toast.makeText(this, "You clicked refresh", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 }
