e54f7fbf0d08f2af35903ebc00f7b77c6257a58f
MontyYan <839343988@qq.com>
2018-12-12 14:22:45 +0800

    Test commit.

--- app/src/main/java/com/focjoe/roucator/LogInActivity.java
+++ app/src/main/java/com/focjoe/roucator/LogInActivity.java
@@ -2,6 +2,7 @@ package com.focjoe.roucator;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class LogInActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class LogInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        Toast.makeText(this, "SDSDSSD", Toast.LENGTH_SHORT).show();
     }
 }
